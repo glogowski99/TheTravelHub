194ae99f1ae991d1d789dd7c592a06430d77d446
DonnCycu <kglogowski99@gmail.com>
2023-09-21 21:38:14 +0200

    division into components and creation of HomeView and files belonging to it

---
+++ src/components/home/HomePage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="t-relative t-h-screen t-w-screen">
+    <div class="t-relative">
+      <yellow-stein class="t-absolute t-right-0 t-top-0"/>
+    </div>
+    <div class="t-flex t-pt-40 t-h-full t-container t-mx-auto">
+      <div class="t-flex t-flex-col t-items-start t-w-1/2 raleway">
+        <p class="t-font-bold t-text-base t-tracking-wider t-text-dark-orange t-uppercase">
+          Find all information in one place
+        </p>
+        <h1 class="t-font-bold t-tracking-wide t-text-font-black">
+          <span class="t-text-dark-orange">Travel</span>
+          with the information <br> you need
+        </h1>
+        <p class="t-mt-8 t-text-black t-text-sm t-tracking-wide">
+          Search for information about the weather in a given country, <br> its currency or see the best hotels and flights.
+        </p>
+        <router-link
+            to="/"
+            class="t-mt-2 t-px-10 t-py-3 t-bg-background-button t-text-white t-shadow-md t-rounded-lg hover:-t-translate-y-1/4 t-transition-transform"
+        >
+          Find More
+        </router-link>
+      </div>
+      <div class="t-relative t-w-1/2">
+        <img
+            alt="Tourist"
+            src="@/assets/home-page-image.png"
+            class="t-absolute t-top-0 t-left-0 t-right-0 t-bottom-0 t-h-[500px]"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import YellowStein from "@/components/home/YellowStein";
+export default {
+  components: {YellowStein}
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/home/OtherThinks.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="t-relative">
+    <yellow-stein class="t-absolute t-left-[-20%] t-top-0 t-rotate-[220deg]"/>
+    <div class="t-container t-mx-auto">
+      <p class="t-text-center raleway">
+        something for you
+      </p>
+      <h3 class="t-mt-2 t-text-center t-text-neutral-800 t-font-semibold t-capitalize">
+        find something for yourself
+      </h3>
+      <div class="t-flex t-items-center t-justify-between t-my-20">
+        <div
+            class="t-flex t-flex-col t-items-center t-justify-between t-p-6 t-w-72 t-h-72 t-bg-white t-rounded-lg t-shadow"
+            v-for="(item, index) in boxItems"
+            :key="index"
+        >
+          <img
+              class="t-w-24 t-h-24"
+              :src="item.boxImage"
+              alt="Hotel png image"
+          />
+          <span
+              class="t-text-xl t-font-semibold t-text-font-black releway t-tracking-wide"
+          >
+          {{ item.boxTitle }}
+        </span>
+          <span
+              class="t-text-center"
+          >
+          {{ item.boxDescription }}
+        </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+import YellowStein from "@/components/home/YellowStein";
+
+export default {
+  components: {YellowStein},
+  setup(){
+    const boxItems = ref([
+      {
+        boxImage: require("@/assets/airplane.png"),
+        boxTitle: 'Best Flights',
+        boxDescription: 'We have prepared a list of flights that will meet your expectations!',
+      },
+      {
+        boxImage: require("@/assets/hotel.png"),
+        boxTitle: 'Best Hotels',
+        boxDescription: 'A list of all hotels and their reviews in the country you want to go to',
+      },
+      {
+        boxImage: require("@/assets/weather-app.png"),
+        boxTitle: 'Calculated Weather',
+        boxDescription: 'Are you worried about the weather in the country you are traveling to? We will help you check it!',
+      },
+      {
+        boxImage: require("@/assets/money-exchange.png"),
+        boxTitle: 'Currency converter',
+        boxDescription: 'There are many currencies in the world depending on the country. You can check the conversion rate for your currency here!',
+      },
+    ])
+    return{
+      boxItems
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
