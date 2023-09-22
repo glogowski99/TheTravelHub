ecf4c100976c14d7f62a4c8aacbc65d8558b2463
DonnCycu <kglogowski99@gmail.com>
2023-09-22 21:38:33 +0200

    adding fontawesome and configuring files

--- src/main.js
+++ src/main.js
@@ -7,4 +7,10 @@ import './assets/tailwind.css'
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faChevronDown, faChevronUp)
+
+createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
